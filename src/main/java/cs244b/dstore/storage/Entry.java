647d87aa01c72bc07f3ccfcdeecb47a215c51748
wctaiwan <wctaiwan@gmail.com>
2014-11-12 16:20:01 -0800

    create and delete

--- src/main/java/cs244b/dstore/storage/Entry.java
+++ src/main/java/cs244b/dstore/storage/Entry.java
@@ -3,4 +3,9 @@ package cs244b.dstore.storage;
 public class Entry {
     public String value;
     public int version;
+
+    public Entry (String value) {
+        this.value = value;
+        this.version = 0;
+    }
 }
